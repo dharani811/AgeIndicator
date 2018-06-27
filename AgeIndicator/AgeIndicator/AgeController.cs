0c31955a4adc5d13c0a48e28c98de3bb0f92d91e
dharani811 <dharani811@gmail.com>
2018-06-27 15:48:20 +0530

    Initial commit

---
+++ AgeIndicator/AgeIndicator/AgeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DK.AgeIndicator
+{
+    public class AgeController:INotifyPropertyChanged
+    {
+        private string age;
+        private readonly DateTime dob = new DateTime(1989, 11, 9, 11, 51, 40);
+
+        public AgeController()
+        {
+            Task.Run(() => TriggerAge());
+        }
+
+        public string Age { get => age; set => age = value; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void TriggerAge()
+        {
+            while (true)
+            {
+                System.Windows.Application.Current.Dispatcher.Invoke(() => {
+
+                    //get age now
+                    age = CalculateYourAge();
+
+                    //notify UI
+                    if (PropertyChanged != null)
+                        PropertyChanged(this, new PropertyChangedEventArgs("Age"));
+
+                    }
+                    );
+                Task.Delay(1000).Wait();
+            }
+        }
+
+        /// <summary>  
+        /// For calculating age  
+        /// </summary>  
+        /// <param name="Dob">Enter Date of Birth to Calculate the age</param>  
+        /// <returns> years, months,days, hours...</returns>  
+        private string CalculateYourAge()
+        {
+            DateTime Now = DateTime.Now;
+            int Years = new DateTime(DateTime.Now.Subtract(dob).Ticks).Year - 1;
+            DateTime PastYearDate = dob.AddYears(Years);
+            int Months = 0;
+            for (int i = 1; i <= 12; i++)
+            {
+                if (PastYearDate.AddMonths(i) == Now)
+                {
+                    Months = i;
+                    break;
+                }
+                else if (PastYearDate.AddMonths(i) >= Now)
+                {
+                    Months = i - 1;
+                    break;
+                }
+            }
+            int Days = Now.Subtract(PastYearDate.AddMonths(Months)).Days;
+            int Hours = Now.Subtract(PastYearDate).Hours;
+            int Minutes = Now.Subtract(PastYearDate).Minutes;
+            int Seconds = Now.Subtract(PastYearDate).Seconds;
+            return String.Format("Age: {0} Year(s) {1} Month(s) {2} Day(s) {3} Hour(s) {4} Second(s)",
+            Years, Months, Days, Hours, Seconds);
+        }
+    }
+}
---
+++ AgeIndicator/AgeIndicator/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using DK.AgeIndicator;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AgeIndicator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new AgeController();
+            this.Left = DK.AgeIndicator.Properties.Settings.Default.Left;
+            this.Top = DK.AgeIndicator.Properties.Settings.Default.Top;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Window_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            MenuItem closeForm = new MenuItem();
+            closeForm.Header = "Close";
+            closeForm.Click += CloseForm_Click;
+            ContextMenu cm = new ContextMenu();
+            cm.Items.Add(closeForm);
+            this.ContextMenu = cm;
+        }
+
+        private void CloseForm_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            DK.AgeIndicator.Properties.Settings.Default.Top = this.Top;
+            DK.AgeIndicator.Properties.Settings.Default.Left = this.Left;
+            DK.AgeIndicator.Properties.Settings.Default.Save();
+        }
+    }
+}
